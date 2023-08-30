dd750a55c7381ba917d8478d511bc14f41fa58ca
BatalinVlad <vladbatalinontech@gmail.com>
2023-08-30 17:17:36 +0300

    more styles

--- frontend/src/cmps/BoardsList.js
+++ frontend/src/cmps/BoardsList.js
@@ -28,19 +28,14 @@ const BoardsList = ({ boards, templateBoards, user }) => {
             {
                 user &&
                 <div>
-                    <div className="boards-list-main-inner-container-wrapper">
+                    <div className="boards-list-container">
                         {(myBoards.length === 0) ?
                             <p> you haven't created a board yet...</p> :
                             <div className="boards-list-main-inner-container">
-
-                                <div>
-                                    <p className="capitalize">created boards</p>
-                                    <hr />
-                                </div>
-
-                                <div className="boards-list-main-inner-container-grid flex justify-center column">
+                                <p className="capitalize">created boards</p>
+                                <div className="boards-container flex row">
                                     {myBoards.map(myBoard => (
-                                        <NavLink className="boards-list-main-inner-container-grid-item" key={myBoard._id} to={`/board/${'board'}/${myBoard._id}`}>
+                                        <NavLink className="pointer" key={myBoard._id} to={`/board/${'board'}/${myBoard._id}`}>
                                             <BoardPreview board={myBoard} />
                                         </NavLink>
                                     ))}
@@ -48,19 +43,17 @@ const BoardsList = ({ boards, templateBoards, user }) => {
                             </div>}
                     </div>
 
-                    <div className="boards-list-main-inner-container-wrapper">
+                    <div className="boards-list-container">
                         <div className="boards-list-main-inner-container">
                             <p className="capitalize"> boards you collaborate on </p>
-                            <hr />
-
                             {(myCollaboratedBoards.length === 0) ?
                                 <div className="fill-width text-center">
                                     <span className="caspanitalize"> you're not collaborating on any boards yet... </span>
                                 </div>
                                 :
-                                <div className="boards-list-main-inner-container-grid flex justify-center column">
+                                <div className="boards-list-container">
                                     {myCollaboratedBoards.map(myCollaboratedBoard => (
-                                        <NavLink className="boards-list-main-inner-container-grid-item" key={myCollaboratedBoard._id} to={`/board/${myCollaboratedBoard._id}`}>
+                                        <NavLink className="pointer" key={myCollaboratedBoard._id} to={`/board/${myCollaboratedBoard._id}`}>
                                             <BoardPreview board={myCollaboratedBoard} />
                                         </NavLink>
                                     ))}
