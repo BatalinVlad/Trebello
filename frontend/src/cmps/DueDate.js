70372d0092da8b053386a561927bbb86b2bad703
Batalin Vlad <55594257+BatalinVlad@users.noreply.github.com>
2023-10-25 23:15:31 +0300

    remove logs

--- frontend/src/cmps/DueDate.js
+++ frontend/src/cmps/DueDate.js
@@ -3,7 +3,6 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const DueDate = (props) => {
-    console.log(props);
     const [dueDate, setDueDate] = useState(null);
 
     useEffect(() => {
--- frontend/src/pages/Board.js
+++ frontend/src/pages/Board.js
@@ -147,7 +147,6 @@ const Board = () => {
 
 
   const updateBoardHandler = (board, msg, notificationType) => {
-    console.log('update board');
     if (filteredBoard) return;
     dispatch(updateBoard(board, msg, notificationType));
   }
